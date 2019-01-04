8ac8718a50115f84c93f3e992848a1735624bb9f
Charles Martin Reed <charlesmartinreed@icloud.com>
2019-01-04 17:51:37 -0600

    Create prototype UICollectionViewCell and associated outlets.

---
+++ Card Drop/CategoryCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Card Drop
+//
+//  Created by Charles Martin Reed on 1/4/19.
+//  Copyright © 2019 Charles Martin Reed. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+    //MARK:- @IBOutlets
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    
+}
