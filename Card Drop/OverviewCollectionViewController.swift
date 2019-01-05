58acadbbb70a7c53956955d0e64d61092323bdf7
Charles Martin Reed <charlesmartinreed@icloud.com>
2019-01-04 18:22:49 -0600

    Implement rounded corners and proper edge insets for custom collection view cells.

--- Card Drop/OverviewCollectionViewController.swift
+++ Card Drop/OverviewCollectionViewController.swift
@@ -8,82 +8,81 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
 class OverviewCollectionViewController: UICollectionViewController {
-
+    
+    //MARK:- Properties
+    //using the helper functions Brian created
+    //generic dataRequest, takes the Category type and loades data from the dataSource, a plist named "Categories"
+    let categoryDataRequest = DataRequest<Category>(dataSource: "Categories")
+    var categoryData = [Category]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadData()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    
+    func loadData() {
+        categoryDataRequest.getData { [weak self] dataResult in
+            //request is success or failure
+            switch dataResult {
+            case .failure:
+                print("Could not load categories")
+            case .success(let categories):
+                self?.categoryData = categories
+                self?.collectionView.reloadData() //using this for layout
+            }
+        }
     }
-    */
+}
 
-    // MARK: UICollectionViewDataSource
+  
 
+//MARK:- CollectionView Datasource methods
+extension OverviewCollectionViewController {
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
-
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        return categoryData.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
+        
+        //configuring the cells - unwrapping because we need a non-optional value here
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? CategoryCollectionViewCell else { fatalError("Could not create category cell for collection view") }
+        
+        let category = categoryData[indexPath.item]
+        guard let image = UIImage(named: category.categoryImageName) else { fatalError("Could not load image for cell")}
+        cell.backgroundImageView.image = image
+        cell.categoryLabel.text = category.categoryName
+        
         return cell
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
-
 }
+
+//MARK:- CollectionView Delegate methods
+extension OverviewCollectionViewController {
+    override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+        //in this function, we can set the display properties for the custom cell
+        cell.layer.cornerRadius = 14
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+}
+
+//MARK:- CollectionView Layout Delegate
+extension OverviewCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        //fixing the spacing issues between the individual cells
+        return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        
+    }
+}
+
+
