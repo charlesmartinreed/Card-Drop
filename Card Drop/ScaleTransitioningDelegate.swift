232d0c94d8548de3459169878f390322702195f9
Charles Martin Reed <charlesmartinreed@icloud.com>
2019-01-05 18:49:09 -0600

    Prepare container view for creating custom animation.

--- Card Drop/ScaleTransitioningDelegate.swift
+++ Card Drop/ScaleTransitioningDelegate.swift
@@ -12,12 +12,23 @@
 
 import UIKit
 
+//our overiew and image selection controllers will adopt this protocol to get the imageView
+protocol Scaling {
+    func scaling(transition: ScaleTransitioningDelegate) -> UIImageView?
+}
+
+enum TransitionState {
+    case begin
+    case end
+}
+
 class ScaleTransitioningDelegate: NSObject {
     
     //MARK:- Animation properties
     let animationDuration = 0.5
 }
 
+//MARK:- UIViewControllerAnimatedTransitioning delegate methods
 extension ScaleTransitioningDelegate : UIViewControllerAnimatedTransitioning {
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
         return animationDuration
@@ -25,6 +36,33 @@ extension ScaleTransitioningDelegate : UIViewControllerAnimatedTransitioning {
     
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
         
+        //grab the container view
+        let containerView = transitionContext.containerView
+        
+        //grabe overview VC and image selection view controllers
+        guard let backgroundVC = transitionContext.viewController(forKey: .from) else { return }
+        guard let foregroundVC = transitionContext.viewController(forKey: .to) else { return }
+        
+        //this works because anything that adopts the Scaling protocol must utilize this function, which in turn optionally returns an imageView. We'll use that image at the start and end of animation.
+        guard let backgroundImageView = (backgroundVC as? Scaling)?.scaling(transition: self) else { return }
+        guard let foregroundImageView = (backgroundVC as? Scaling)?.scaling(transition: self) else { return }
+        
+        let imageViewSnapshot = UIImageView(image: backgroundImageView.image)
+        imageViewSnapshot.contentMode = .scaleAspectFill
+        imageViewSnapshot.layer.masksToBounds = true
+        
+        //at the outset of the animation, we'll need to hide both of these
+        backgroundImageView.isHidden = true
+        foregroundImageView.isHidden = true
+        
+        let foregroundBGColor = foregroundVC.view.backgroundColor
+        foregroundVC.view.backgroundColor = UIColor.clear
+        containerView.backgroundColor = UIColor.white
+        
+        //populating our containerView, from bottom to top
+        containerView.addSubview(backgroundVC.view)
+        containerView.addSubview(foregroundVC.view)
+        containerView.addSubview(imageViewSnapshot)
         
     }
     
